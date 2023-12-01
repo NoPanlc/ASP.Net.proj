db61afc276d6b06be47591debd2542f7b38c2668
Daniyar <mamindani@gmail.com>
2023-12-01 21:09:59 +0200

    Task 4.5

--- Weather.Server/Controllers/WeatherForecastController.cs
+++ Weather.Server/Controllers/WeatherForecastController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using System.Text;
 using Weather.Server.DTOs;
 
 namespace Weather.Server.Controllers
@@ -10,6 +12,7 @@ namespace Weather.Server.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly OpenWeather _openWeather;
+        private readonly HttpClient _httpClient;
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,8 +21,10 @@ namespace Weather.Server.Controllers
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger,
-            IOptions<OpenWeather> openWeather)
+            IOptions<OpenWeather> openWeather,
+            HttpClient httpClient)
         {
+            _httpClient = httpClient;
             _openWeather = openWeather.Value;
             _logger = logger;
         }
@@ -36,12 +41,59 @@ namespace Weather.Server.Controllers
             .ToArray();
         }
         [HttpGet("CurrentWeather")]
-        public CurrentWeatherDTO GetCurrentWeather([FromQuery][Required] string cityName,
-                                                  [FromQuery] string stateCode,
+        public async Task<ActionResult<CurrentWeatherDTO>> GetCurrentWeather([FromQuery][Required] string cityName, 
+                                                   [FromQuery] int? stateCode,
                                                   [FromQuery] int? countryCode)
         {
-            return new CurrentWeatherDTO();
+            if (_openWeather == null || string.IsNullOrWhiteSpace(cityName))
+            {
+                return BadRequest("Some configuration or request is empty"); 
+            }
+
+           
+            StringBuilder geocode = new StringBuilder();
+            string geocodeUrl = geocode.Append(_openWeather.Site + _openWeather.GeoResponseType + _openWeather.GeoVersion)
+                      .Append(_openWeather.GeolocationTemplate.Replace("cityname", cityName)
+                      .Replace(",stateCode", stateCode.HasValue ? stateCode.Value.ToString() : "")
+                      .Replace(",countrycode", countryCode.HasValue ? countryCode.Value.ToString() : "")
+                      .Replace("APIKey", _openWeather.Key)).ToString();
+
+            var geoResponse = await _httpClient.GetAsync(geocodeUrl); 
+
+            if (!geoResponse.IsSuccessStatusCode || geoResponse == null || geoResponse.Content == null)
+            {
+                return BadRequest("Call to Open Weather for geocode failed");
+            }
+
+            string geo = await geoResponse.Content.ReadAsStringAsync(); 
+            var geoCode = JsonConvert.DeserializeObject<GeoCodeDTO>(geo);
+
+            if (geoCode == null)
+            {
+                return BadRequest("Deserialization of geocode failed");
+            }
+
+          
+            StringBuilder currentWeatherUrl = new StringBuilder();
+            currentWeatherUrl.Append(_openWeather.Site + _openWeather.WeatherResponseType + _openWeather.WeatherVersion)
+                             .Append(_openWeather.CurrentWeatherTemplate.Replace("=lat", "=" + geoCode.Lat)
+                             .Replace("=lon", "=" + geoCode.Lon).Replace("APIKey", _openWeather.Key)).ToString();
+
+            var currentWeatherResponse = await _httpClient.GetAsync(geocodeUrl); 
+            if (!currentWeatherResponse.IsSuccessStatusCode || currentWeatherResponse == null || currentWeatherResponse.Content == null)
+            {
+                return BadRequest("Call to Open Weather for current weather failed");
+            }
+
+            string current = await geoResponse.Content.ReadAsStringAsync(); 
+            var currentWeather = JsonConvert.DeserializeObject<CurrentWeatherDTO>(current);
+            if (currentWeather == null)
+            {
+                return BadRequest("Deserialization of current weather failed");
+            }
+
+            return Ok(currentWeather);
         }
     }
 }
-}
+
