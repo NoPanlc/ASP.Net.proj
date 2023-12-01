5c5ab266f8a77a06c4bbfc2755fd26d1f99478c2
Daniyar <mamindani@gmail.com>
2023-12-01 20:39:25 +0200

    Task 4.2

---
+++ Weather.Server/DTOs/CurrentWeatherDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+
+namespace Weather.Server.DTOs
+{
+    public class CurrentWeatherDTO
+    {
+        public Main? Main { get; set; }
+        public Wind? Wind { get; set; }
+        public Clouds? Clouds { get; set; }
+        public double Dt { get; set; }
+    }
+    public class Main
+    {
+        public int Temp { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+    }
+    public class Wind
+    {
+        public double Speed { get; set; }
+    }
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+}
