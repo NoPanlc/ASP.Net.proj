a4ad31e82c9f73f39c0ec813d963acec61de4b70
Daniyar <mamindani@gmail.com>
2023-12-25 20:21:42 +0200

    Task 6.5 and 6.6

---
+++ Weather.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Weather.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
